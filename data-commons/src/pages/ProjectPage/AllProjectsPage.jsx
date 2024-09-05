9f4f77fd1a07efb08c6690a0f01132ef2c6b3c62
Jeremy Lau <jeremyhllau@gmail.com>
2024-09-05 20:43:52 +1000

    Added backend intergration for projects, patients and datasets

--- data-commons/src/pages/ProjectPage/AllProjectsPage.jsx
+++ data-commons/src/pages/ProjectPage/AllProjectsPage.jsx
@@ -1,6 +1,4 @@
-import AllProjects from "../../components/Project/AllProjects";
 import BackendProjects from "../../components/Project/BackendProjects";
-import SimpleBackend from "../../components/Project/SimpleBackend";
 
 
 export default function AllProjectsPage() {
